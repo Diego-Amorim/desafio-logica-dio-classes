e859ed6755d274bf5d347bf37e6ff7c14f83aaef
Diego-Amorim <diego.aiafx@gmail.com>
2023-12-08 17:28:39 -0300

    Teste de Atualização

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+class hero{
+    constructor(nameHero, ageHero,classHero){
+        this.nameHero = nameHero
+        this.ageHero = ageHero
+        this.classHero = classHero
+    }
+}
+
+let mage = new hero("Sirius","22","Mage")
+
+console.log(mage)
